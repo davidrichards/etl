4735661158564e1157dc64e3f46156c63cb0fe6c
David Richards <davidlamontrichards@gmail.com>
2009-08-09 14:10:55 -0600

    Ignoring the RDF stuff until it is ready.

--- lib/etl/rdf_2_data_frame.rb
+++ lib/etl/rdf_2_data_frame.rb
@@ -1,19 +1,19 @@
 # Requires data_frame (sudo gem install davidrichards-data_frame)
-gem 'data_frame'
-require 'data_frame'
-
-# This is a simple tool that converts RDF to DataFrames.  It uses the
-# subjects as the rows, the objects as the columns, and the predicates
-# as the values.  This can make the data much more accessible by more
-# analysis tools. 
-class RDF2DataFrame < ETL
-  
-  def extract
-    source = self.options.fetch(:source, nil)
-    @raw = OpenContent::Extractor.process(source, RDF2DataFrame.logger)
-  end
-  
-  def transform
-    # TODO
-  end
-end
+# gem 'davidrichards-data_frame'
+# require 'data_frame'
+# 
+# # This is a simple tool that converts RDF to DataFrames.  It uses the
+# # subjects as the rows, the objects as the columns, and the predicates
+# # as the values.  This can make the data much more accessible by more
+# # analysis tools. 
+# class RDF2DataFrame < ETL
+#   
+#   def extract
+#     source = self.options.fetch(:source, nil)
+#     @raw = OpenContent::Extractor.process(source, RDF2DataFrame.logger)
+#   end
+#   
+#   def transform
+#     # TODO
+#   end
+# end
